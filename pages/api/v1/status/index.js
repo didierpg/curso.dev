033b350421ff34ec6518f7502c27ef923b984be2
Didier Ferreira <didierpg@gmail.com>
2024-09-09 19:09:25 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("select 1 + 1;");
+  console.log(result);
+
   response.status(200).json("áéíóú");
 }
 
