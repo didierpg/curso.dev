3a5f292949bd19ded9b0c29725b3efb8fb652de5
Didier Ferreira <didierpg@gmail.com>
2024-11-05 12:45:42 -0300

    fix: adjust image import in `App`component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Image from "next/image";
+import underConstructionImage from "assets/underconstruction.svg";
 
 const App = () => {
   return (
     <div style={styles.container}>
-      <img
-        src="https://cdn.gencraft.com/prod/user/9f13054e-8b6a-46d9-a4ba-95c753ffad79/0f94083e-ef77-4f42-9e49-f14ea6e96642/image/image1_0.jpg?Expires=1726924009&Signature=nd0QLMCk63eTwdpaxJrRMWlVLqJ2X3wgHP8u3EQ7xsFnJWvfHSNGQyX3~qMQ88m4yxR6av1lO3VNT6fnNHOvxbz-ePjr0rZwZtyAoKHx9GvtnfJJDDwhMWQIaQ6GxTTRiF~j8UjM0fWXZCloARhMFDi3kQuyqTQy7SAn1jZzMok3hApbAYCxno6aLnS~96mgGiiZ9wq~-YAh8a3o~PmEc8yDcckL6rWoKMbxdJ~JO1VvcIk1dPzaBFVfLfoa6Pl9ToZPjTf9-zN7Jm6pFcjjBgjncBVk~7Wow-uxpr9MprmGSt92nbt3pxq7QtMyehDPtyPR5EV7EnMbKwdTokteyg__&Key-Pair-Id=K3RDDB1TZ8BHT8"
+      <Image
+        src={underConstructionImage}
         alt="Em construção"
         style={styles.image}
       />
