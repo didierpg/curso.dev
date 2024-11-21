26336529cfea8273f8380fa57b5d796148f9d0b9
Didier Ferreira <didierpg@gmail.com>
2024-11-21 17:57:29 +0000

    fix: show `DatabaseStatus` loading message correctly

--- pages/status/index.js
+++ pages/status/index.js
@@ -8,11 +8,11 @@ async function fetchAPI(key) {
 
 export default function StatusPage() {
   return (
-    <div style={styles.pageContainer}>
-      <h1 style={styles.title}>Status</h1>
+    <>
+      <h1>Status</h1>
       <UpdatedAt />
       <DatabaseStatus />
-    </div>
+    </>
   );
 }
 
@@ -27,12 +27,7 @@ function UpdatedAt() {
     updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
   }
 
-  return (
-    <div style={styles.statusItem}>
-      <span style={styles.statusLabel}>Última atualização:</span>
-      <span>{updatedAtText}</span>
-    </div>
-  );
+  return <div>Última atualização: {updatedAtText}</div>;
 }
 
 function DatabaseStatus() {
@@ -40,74 +35,26 @@ function DatabaseStatus() {
     refreshInterval: 2000,
   });
 
-  const carregando = "Carregando...";
+  let databaseStatusInformation = "Carregando...";
+
+  if (!isLoading && data) {
+    databaseStatusInformation = (
+      <>
+        <div>Versão: {data.dependencies.database.version}</div>
+        <div>
+          Conexões abertas: {data.dependencies.database.opened_connections}
+        </div>
+        <div>
+          Conexões máximas: {data.dependencies.database.max_connections}
+        </div>
+      </>
+    );
+  }
 
   return (
-    <div style={styles.section}>
-      <h2 style={styles.sectionTitle}>Database</h2>
-      <div style={styles.statusItem}>
-        <span style={styles.statusLabel}>Versão:</span>
-        <span>
-          {isLoading ? carregando : data.dependencies.database.postgres_version}
-        </span>
-      </div>
-      <div style={styles.statusItem}>
-        <span style={styles.statusLabel}>Conexões abertas:</span>
-        <span>
-          {isLoading
-            ? carregando
-            : data.dependencies.database.opened_connections}
-        </span>
-      </div>
-      <div style={styles.statusItem}>
-        <span style={styles.statusLabel}>Conexões máximas:</span>
-        <span>
-          {isLoading ? carregando : data.dependencies.database.max_connections}
-        </span>
-      </div>
-    </div>
+    <>
+      <h2>Database</h2>
+      <div>{databaseStatusInformation}</div>
+    </>
   );
 }
-
-const styles = {
-  pageContainer: {
-    maxWidth: "800px",
-    margin: "0 auto",
-    backgroundColor: "#ffffff",
-    borderRadius: "8px",
-    padding: "20px",
-    boxShadow: "0 4px 20px rgba(0, 0, 0, 0.1)",
-    fontFamily: "'Roboto', sans-serif",
-    color: "#333",
-  },
-  title: {
-    fontSize: "2.5rem",
-    color: "#2D3748",
-    marginBottom: "20px",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  sectionTitle: {
-    fontSize: "1.75rem",
-    marginBottom: "15px",
-    color: "#4A5568",
-    fontWeight: "600",
-  },
-  statusItem: {
-    display: "flex",
-    justifyContent: "space-between",
-    marginBottom: "12px",
-    padding: "15px",
-    backgroundColor: "#F7FAFC",
-    borderRadius: "6px",
-    fontSize: "1rem",
-    transition: "background-color 0.3s ease",
-  },
-  statusLabel: {
-    fontWeight: "bold",
-    color: "#4A5568",
-  },
-  section: {
-    marginTop: "25px",
-  },
-};
